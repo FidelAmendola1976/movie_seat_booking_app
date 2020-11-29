7e767f117cf41048f7e97a075568f1133aef4008
Pablo Federico Fidel Amendola <fidelamendola@yahoo.com>
2020-11-29 19:56:03 -0300

    simple app using Vanilla Javascript, CSS and HTML

---
+++ script.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const counts = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+const ticketPrice = +movieSelect.value;
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  counts.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie click event
+
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+// Seat click event
+
+container.addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
